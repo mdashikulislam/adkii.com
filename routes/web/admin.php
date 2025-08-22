2dd92d7e6fa410ba16554b6cc710f895c7395633
mdashikulislam <ashik.nwu@gmail.com>
2025-08-23 00:53:48 +0600

    add categry countroller

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\RolePermissionController;
 use App\Http\Controllers\Admin\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::prefix('admin')->name('admin.')->middleware(['auth','verified'])->group(function () {
+    //CategoryController
+    Route::resource('categories', CategoryController::class);
 
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
